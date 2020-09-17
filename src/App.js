dd63c209e25c38604e9e7525f146ab58da8f052e
kubo550 <43968748+kubo550@users.noreply.github.com>
2020-09-17 16:33:15 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ButtonGrid from './components/ButtonsGrid';
+
 
 function App() {
+  let r,g,b, color
+  const user = 'admin';
+  const colorsArr = ['red', 'green', 'yellow', 'blue', 'orange', 'violet', 'grey', 'white'];
+
+  const newRandomColor = () => {
+      r = Math.floor(Math.random() * 255);
+      g = Math.floor(Math.random() * 255);
+      b = Math.floor(Math.random() * 255);
+      color = `rgb(${r},${g},${b})`;
+  }
+
+  newRandomColor();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Classify This Color!</h1>
+      <div 
+        className="random-color"
+        style={{background:color}}>
+          bg: {color}
+        </div>
+      <ButtonGrid user={user} colorsArr={colorsArr} />
     </div>
   );
 }
